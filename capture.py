18f02af52db3599883f7ab7b4fb63067da605eba
wzh99 <wzh1999_frog@126.com>
2019-10-29 16:52:46 +0800

    Image sequence recording program.

--- capture.py
+++ capture.py
@@ -1,15 +1,26 @@
+from collections import deque
+from typing import List, Union
+
+import cv2
 import numpy as np
 import pyrealsense2 as rs
+
+import gesture
+import preproc
 
 # Capture
 image_width = 640
 image_height = 480
 fps = 30
+start_diff_threshold = 10  # mean of test dequeue indicating when the recording should begin
+finish_diff_threshold = 10  # mean of test dequeue indicating when the recording should end
+min_seq_length = 10
+test_deque_size = 4  # size of frames temporarily stored in test dequeue
 
 
 class Camera:
     """
-    Intel RealSense camera for use in image capture.
+    Simple wrapper of Intel RealSense camera in image capture.
     """
 
     def __init__(self):
@@ -27,7 +38,7 @@ class Camera:
     def __del__(self):
         self.pipeline.stop()
 
-    def capture(self) -> [np.ndarray]:
+    def capture(self) -> List[np.ndarray]:
         """
         Capture an coherent pair of depth and color image.
         :return: list[depth_image, color_image]
@@ -38,9 +49,153 @@ class Camera:
         depth_frame = aligned_frames.get_depth_frame()
         color_frame = aligned_frames.get_color_frame()
         if not depth_frame or not color_frame:
-            return None, None
+            return []
 
-        # Convert images to np.array
+        # Convert images to numpy array
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
         return [depth_image, color_image]
+
+
+class Recorder:
+    """
+    A video recorder that can record frame sequences for dataset creation and realtime capturing.
+    """
+
+    def __init__(self, camera: Camera, path: str = "./"):
+        """
+        Constructor
+        :param camera: Camera object
+        :param path: where to put dataset files
+        """
+        # Set basic members
+        self.gesture = 0  # which gesture to record
+        self.camera = camera
+        self.path = path
+        self.is_recording = False
+
+        # Create recorder GUI
+        self.window_name = "Recorder"
+        cv2.namedWindow(self.window_name, cv2.WINDOW_AUTOSIZE)
+
+        def on_track_bar(x: int):
+            self.gesture = x
+
+        cv2.createTrackbar("Gesture", self.window_name, self.gesture, len(gesture.category_names) - 1,
+                           on_track_bar)
+
+        # Initialize frame buffer
+        self.frame_test_deque = deque(maxlen=test_deque_size)  # store history frames
+        self.diff_test_deque = deque(maxlen=test_deque_size)  # store history mean values
+
+    def record(self) -> Union[np.ndarray, None]:
+        """
+        Record a video sequence as train dataset and store it into directory.
+        :return: numpy array if recording real time test data, or None if creating dataset.
+        """
+        enabled = False  # ready to detect new sequence or not
+        while cv2.waitKey(1) != 27:
+            captured_frame = self.camera.capture()
+            if len(captured_frame) == 0:
+                continue
+            seg_frame = preproc.segment_one_frame(captured_frame, self.camera.depth_scale)
+            self._display(seg_frame)
+            if cv2.waitKey(1) == 32:
+                print("disabled" if enabled else "enabled")
+                enabled = not enabled
+            if enabled:
+                self._try_record_frame(seg_frame)
+
+        return None
+
+    def _try_record_frame(self, frame: List[np.ndarray]) -> Union[List[np.ndarray], None]:
+        """
+        Use mean of mask difference to test whether to start or end recording.
+        :param frame: a single frame to be recorded
+        :return: None
+        """
+        # Get two images in a frame
+        depth_image, gradient_image = frame
+
+        # Pop elements if deque is already full
+        if len(self.frame_test_deque) >= test_deque_size:
+            self.frame_test_deque.popleft()
+            self.diff_test_deque.popleft()
+
+        # Push current depth image to deque
+        this_diff = 0
+        if len(self.frame_test_deque) > 0:
+            last_depth_image = self.frame_test_deque[-1][0]  # get last depth image
+            diff_image = depth_image - last_depth_image
+            this_diff = cv2.mean(cv2.inRange(diff_image, 10, 255))[0]
+            # print(this_diff)
+        self.frame_test_deque.append(frame)
+        self.diff_test_deque.append(this_diff)
+
+        # Compute average difference in recent frames
+        mean = np.mean(self.diff_test_deque)
+        if mean > start_diff_threshold and not self.is_recording:
+            self._start_record()
+        if self.is_recording:
+            if mean < finish_diff_threshold:
+                sequence = self._finish_record()
+                return sequence if self._check_sequence(sequence) else None
+            else:
+                self.depth_store_list.append(depth_image)
+                self.gradient_store_list.append(gradient_image)
+
+        return None
+
+    def _start_record(self):
+        """
+        Start one round of recording
+        :return:
+        """
+        self.is_recording = True
+        # print("Start recording")
+        self.depth_store_list = [frame[0] for frame in self.frame_test_deque]
+        self.gradient_store_list = [frame[1] for frame in self.frame_test_deque]
+
+    def _finish_record(self) -> List[np.ndarray]:
+        """
+        End this round of recording and return recorded frame sequences.
+        :return: [depth_sequence, gradient_sequence]
+        """
+        self.is_recording = False
+        # print("Finish recording")
+        return [np.array(self.depth_store_list), np.array(self.gradient_store_list)]
+
+    @staticmethod
+    def _check_sequence(seq: List[np.ndarray]) -> bool:
+        print("length:", seq[0].shape[0])
+        if seq[0].shape[0] < min_seq_length:
+            return False
+        print("valid")
+        return True
+
+    def _display(self, frame: List[np.ndarray]):
+        """
+        Display a single frame of recorded result
+        :param frame: [depth_image, gradient_image]
+        :return: None
+        """
+        # Scale image to a suitable size
+        window_scale = 0.7
+        scaled_width = int(image_width * window_scale)
+        scaled_height = int(image_height * window_scale)
+        stacked = cv2.resize(np.hstack(frame), (2 * scaled_width, scaled_height))
+
+        # Draw dividing lines and center circles
+        cv2.line(stacked, (scaled_width, 0), (scaled_width, scaled_height), 255)
+        cv2.circle(stacked, (int(scaled_width * .5), int(scaled_height * .5)), 2, 255, thickness=-1)
+        cv2.circle(stacked, (int(scaled_width * 1.5), int(scaled_height * .5)), 2, 255, thickness=-1)
+
+        # Put gesture category text
+        cv2.putText(stacked, gesture.category_names[self.gesture], (0, scaled_height),
+                    cv2.FONT_HERSHEY_PLAIN, 1, 255)
+        cv2.imshow(self.window_name, stacked)
+
+
+if __name__ == '__main__':
+    recorder = Recorder(Camera(), path="./data/")
+    recorder.record()
--- preproc.py
+++ preproc.py
@@ -1,7 +1,7 @@
+from typing import List
+
 import cv2
 import numpy as np
-
-from capture import Camera
 
 # Preprocess
 depth_thresh = 0.4  # in meters
@@ -12,89 +12,84 @@ cb_range = (77, 127)  # Cb component range
 min_contour_area = 10000  # minimum acceptable hand contour area
 
 
-class Preprocessor:
-    def __init__(self):
-        pass
+def segment_one_frame(captured_list: List[np.ndarray], depth_scale: float) -> List[np.ndarray]:
+    """
+    Segment out hand in one pair of depth and gradient images
+    :return: [segmented_depth, segmented_gradient]
+    """
+    # Split image list
+    depth_image, color_image = captured_list[0], captured_list[1]
 
-    def segment_one_frame(self, captured: [np.ndarray]) -> [np.ndarray]:
-        """
-        Segment out hand in one pair of depth and gradient images
-        :return: [segmented_depth, segmented_gradient]
-        """
-        # Split image list
-        depth_image, color_image = captured[0], captured[1]
+    # Create mask from depth
+    # Filter out depth that fall out of accepted range
+    depth_mask = cv2.inRange(depth_image, 1, depth_thresh / depth_scale)
+    # Use median filter to smooth jagged edge
+    depth_mask = cv2.medianBlur(depth_mask, median_size)
+    # Expand the mask a bit
+    depth_mask = cv2.dilate(depth_mask, np.ones((dilation_size, dilation_size)))
+    # Fill holes in depth mask
+    depth_mask = fill_hole(depth_mask)
 
-        # Create mask from depth
-        # Filter out depth that fall out of accepted range
-        depth_mask = cv2.inRange(depth_image, 1, depth_thresh / camera.depth_scale)
-        # Use median filter to smooth jagged edge
-        depth_mask = cv2.medianBlur(depth_mask, median_size)
-        # Expand the mask a bit
-        depth_mask = cv2.dilate(depth_mask, np.ones((dilation_size, dilation_size)))
-        # Fill holes in depth mask
-        depth_mask = self.fill_hole(depth_mask)
+    # Create mask from color
+    # De-noise with median filter
+    blurred_color = cv2.medianBlur(color_image, median_size)
+    # Convert to YUV color space
+    ycrcb_image = cv2.cvtColor(blurred_color, cv2.COLOR_BGR2YCrCb)
+    # Split into separate channels
+    ycrcb_split = cv2.split(ycrcb_image)
+    # Create mask according to uv range
+    cr_mask = cv2.inRange(ycrcb_split[1], cr_range[0], cr_range[1])
+    cb_mask = cv2.inRange(ycrcb_split[2], cb_range[0], cb_range[1])
+    color_mask = cr_mask & cb_mask
+    # Fill holes in color mask
+    color_mask = fill_hole(color_mask)
 
-        # Create mask from color
-        # De-noise with median filter
-        blurred_color = cv2.medianBlur(color_image, median_size)
-        # Convert to YUV color space
-        ycrcb_image = cv2.cvtColor(blurred_color, cv2.COLOR_BGR2YCrCb)
-        # Split into separate channels
-        ycrcb_split = cv2.split(ycrcb_image)
-        # Create mask according to uv range
-        cr_mask = cv2.inRange(ycrcb_split[1], cr_range[0], cr_range[1])
-        cb_mask = cv2.inRange(ycrcb_split[2], cb_range[0], cb_range[1])
-        color_mask = cr_mask & cb_mask
-        # Fill holes in color mask
-        color_mask = self.fill_hole(color_mask)
+    # Combine two masks
+    combined_mask = depth_mask & color_mask
 
-        # Combine two masks
-        combined_mask = depth_mask & color_mask
+    # Find contour of the mask
+    contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    hand_contour = None
+    if len(contours) > 0:
+        hand_contour = max(contours, key=cv2.contourArea)
+        # Reject this contour if it's too small
+        if cv2.contourArea(hand_contour) < min_contour_area:
+            hand_contour = None
 
-        # Find contour of the mask
-        contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        hand_contour = None
-        if len(contours) > 0:
-            hand_contour = max(contours, key=cv2.contourArea)
-            # Reject this contour if it's too small
-            if cv2.contourArea(hand_contour) < min_contour_area:
-                hand_contour = None
+    # Draw hand contour as mask if it is found
+    hand_mask = np.zeros(combined_mask.shape, dtype=np.uint8)
+    if hand_contour is not None:
+        poly_points = hand_contour.reshape(len(hand_contour), 2)
+        cv2.fillPoly(hand_mask, [poly_points], 255)
 
-        # Draw hand contour as mask if it is found
-        hand_mask = np.zeros(combined_mask.shape, dtype=np.uint8)
-        if hand_contour is not None:
-            poly_points = hand_contour.reshape(len(hand_contour), 2)
-            cv2.fillPoly(hand_mask, [poly_points], 255)
+    # Show masked depth image
+    scaled_depth = cv2.convertScaleAbs(depth_image, alpha=0.03)
+    masked_depth = cv2.copyTo(scaled_depth, hand_mask)
 
-        # Show masked depth image
-        scaled_depth = cv2.convertScaleAbs(depth_image, alpha=0.03)
-        masked_depth = cv2.copyTo(scaled_depth, hand_mask)
+    # Compute gradient of grayscale image
+    gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+    gradient_image = cv2.Sobel(gray_image, -1, 1, 1, ksize=5)
+    masked_gradient = cv2.copyTo(gradient_image, hand_mask)
 
-        # Compute gradient of grayscale image
-        gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
-        gradient_image = cv2.Sobel(gray_image, -1, 1, 1, ksize=5)
-        masked_gradient = cv2.copyTo(gradient_image, hand_mask)
+    return [masked_depth, masked_gradient]
 
-        return [masked_depth, masked_gradient]
 
-    @staticmethod
-    def fill_hole(mask: np.ndarray) -> np.ndarray:
-        """
-        File holes in the mask
-        :param mask: mask to be filled
-        :return: filled result
-        """
-        shape = np.array(mask.shape)
-        fill = np.zeros(shape + 2, np.uint8)
-        fill[1:shape[0] + 1, 1:shape[1] + 1] = mask.copy()
-        cv2.floodFill(fill, np.zeros(shape + 4, np.uint8), (0, 0), 255)
-        return mask | ~fill[1:shape[0] + 1, 1:shape[1] + 1]
+def fill_hole(mask: np.ndarray) -> np.ndarray:
+    """
+    File holes in the mask
+    :param mask: mask to be filled
+    :return: filled result
+    """
+    shape = np.array(mask.shape)
+    fill = np.zeros(shape + 2, np.uint8)
+    fill[1:shape[0] + 1, 1:shape[1] + 1] = mask.copy()
+    cv2.floodFill(fill, np.zeros(shape + 4, np.uint8), (0, 0), 255)
+    return mask | ~fill[1:shape[0] + 1, 1:shape[1] + 1]
 
 
 if __name__ == '__main__':
+    from capture import Camera
     camera = Camera()
-    proc = Preprocessor()
     while cv2.waitKey(1) != 27:
-        captured = camera.capture()
-        segmented = proc.segment_one_frame(camera.capture())
+        segmented = segment_one_frame(camera.capture(), camera.depth_scale)
         cv2.imshow("Result", np.hstack(segmented))
